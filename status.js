38fa5653a06493485f561c59d1628b36cfcdc536
christoff.dimitar <dimitarchristoff@izyware.com>
2020-11-09 23:29:17 +0000

    add support for http[s] esConfig.hosts entries. format output for automatin interface

--- status.js
+++ status.js
@@ -1,20 +1,31 @@
 const modtask = () => {};
 modtask.all = (queryObject, cb) => {
+    const commands = [
+        '****** You may also try *********',
+        'circuit breaker status: _nodes/stats/breaker?pretty',
+        'list all indices: _cat/indices',
+        '*********************************',
+        ''
+    ];
     const { esConfigId } = queryObject;
+    let cmd = queryObject.options || ''; // `_nodes/stats/breaker?pretty`;
     modtask.doChain([
-        ['chain.importProcessor', 'chain', {
-        verbose: modtask.verbose
-        }],
         ['//inline/rel:json?loadById', { id: esConfigId }],
         chain => {
             const esConfig = chain.get('outcome').data;
-            const url = `http://${esConfig.hosts[0]}/_nodes/stats/breaker?pretty`;
-            modtask.ldmod('features/v2/http').universalHTTP().sendRequest({
-                url
-            }, function(outcome) {
-                const data = outcome.responseText;
-                console.log(data);
-            });
+            let serviceEndPoint = esConfig.hosts[0];
+            if (serviceEndPoint.indexOf('http') != 0) {
+                serviceEndPoint = `http://` + serviceEndPoint;
+            }
+            const url = `${serviceEndPoint}/${cmd}`;
+            console.log(url);
+            chain(['net.httprequest', { url }]);
+        },
+        chain => {
+            var result = chain.get('outcome');
+            var data = result.responseText;
+            data = commands.join('\n') + data;
+            chain(['outcome', { success: true, data }]);
         }
     ]);
 }
